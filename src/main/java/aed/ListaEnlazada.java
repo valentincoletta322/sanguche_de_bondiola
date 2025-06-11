1d550d94afe41293b73b42af18b6d490908a2c53
Clara Wallach <clari.wallach@gmail.com>
2025-06-10 23:46:09 -0300

    modificaciones en lista enlazada y tests lista enlazada

--- src/main/java/aed/ListaEnlazada.java
+++ src/main/java/aed/ListaEnlazada.java
@@ -14,23 +14,26 @@ public class ListaEnlazada<T> {
 
     private Nodo primero;
     private Nodo ultimo;
+    private int longitud; 
 
     public ListaEnlazada() {
         this.primero = null;
         this.ultimo = null;
+        this.longitud = 0;
     }
 
     // Agregar un bloque al final de la lista - O(1)
     public void agregar(T valor) {
         Nodo nuevoNodo = new Nodo(valor);
 
-        if (this.primero == null) {
+        if (this.longitud == 0) {
             primero = nuevoNodo;
             ultimo = nuevoNodo;
         } else {
             ultimo.siguiente = nuevoNodo;
             ultimo = nuevoNodo;
         }
+        this.longitud += 1; 
     }
 
     // Obtener el último bloque - O(1)
@@ -40,4 +43,20 @@ public class ListaEnlazada<T> {
         }
         return ultimo.valor;
     }
+
+    //agregue para poder testear
+
+    public int longitud() {
+        return this.longitud; 
+    }
+
+    public T obtener(int i) {
+        Nodo res = this.primero;
+        for (int n=0; n < i; n++){
+            res = res.siguiente;
+        }
+
+        return res.valor; 
+    }
+
 }
